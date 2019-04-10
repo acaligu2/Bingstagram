e8142dfc4495ffc0c7592feec21acc7aee90128e
Anthony <anthony@Anthonys-Mac.local>
2019-04-10 17:04:51 -0400

    Initial Commit

---
+++ Bingstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bingstagram
+//
+//  Created by Anthony on 4/10/19.
+//  Copyright © 2019 CaligureForno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
