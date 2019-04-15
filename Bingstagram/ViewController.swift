70ff49cb87a156af4a050080e1d1ad618516f6a7
Anthony <anthony@Anthonys-Mac.local>
2019-04-15 13:21:04 -0400

    Added photo-upload functionality

--- Bingstagram/ViewController.swift
+++ Bingstagram/ViewController.swift
@@ -8,11 +8,58 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UICollectionViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    var images = [UIImage]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        title = "Bingstagram"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .camera, target: self, action: #selector(importPicture))
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        
+        return images.count
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageView", for: indexPath)
+        
+        if let imageView = cell.viewWithTag(1000) as? UIImageView {
+         
+            imageView.image = images[indexPath.item]
+            
+        }
+        
+        return cell
+        
+    }
+    
+    @objc func importPicture(){
+        
+        let imageSelection = UIImagePickerController()
+        imageSelection.allowsEditing = true
+        imageSelection.delegate = self
+        present(imageSelection, animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let image = info[.editedImage] as? UIImage else { return }
+        
+        dismiss(animated: true)
+        
+        images.insert(image, at: 0)
+        collectionView?.reloadData()
+        
     }
 
 
